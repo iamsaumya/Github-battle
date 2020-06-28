7eaf97e0e6532c15c1b172c10fbcd289c8919315
Saumya Pandey <saumya@botbakery.io>
2020-06-28 23:28:40 +0530

    Add nav bar

---
+++ app/components/Popular.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function LanguagesNav ({selected,onUpdateLanguage}){
+    const languages = ['All','JavaScript','Ruby','Java','CSS','Python']
+    return (
+        <ul className="flex-center">
+            {
+                languages.map((language) => (<li key={language}>
+                    <button className="btn-clear nav-link"
+                    style={selected === language ? { color : 'rgb(187,46,31)'} : null}
+                    onClick={()=> onUpdateLanguage(language)}>
+                        {language}
+                    </button>
+                </li>)) 
+            }
+        </ul>
+    )
+}
+
+LanguagesNav.propTypes = {
+    selected : PropTypes.string.isRequired,
+    onUpdateLanguage : PropTypes.func.isRequired
+}
+export default class Popular extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            selectedLanguage : "All"
+        }
+        
+        this.updateLanguage = this.updateLanguage.bind(this)
+    }
+
+    updateLanguage(selectedLanguage){
+        this.setState({
+            selectedLanguage
+        })
+    }
+    render(){
+
+        return (
+            <React.Fragment>
+            <LanguagesNav
+            selected = {this.state.selectedLanguage}
+            onUpdateLanguage = {this.updateLanguage}
+                />
+            </React.Fragment>
+        )
+    }
+}
--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-
+import Popular from './components/Popular'
 //Components
 //1. State
 //2. Lifecycle
@@ -9,7 +9,11 @@ import './index.css'
 
 class App extends React.Component{
     render(){
-        return (<div>Hello Saumya!</div>)
+        return (
+            <div className="container">
+                <Popular/>
+            </div>
+        )
     }
 }
 

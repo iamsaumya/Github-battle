755806a42096bd217710abece9a932f4e761a95a
Saumya Pandey <saumya@botbakery.io>
2020-06-29 09:03:02 +0530

    Use react icons and Make grids for repo

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {fetchPopularRepos} from '../utils/api'
+import {FaUser,FaStar,FaCodeBranch,FaExclamationTriangle} from 'react-icons/fa'
 
 function LanguagesNav ({selected,onUpdateLanguage}){
     const languages = ['All','JavaScript','Ruby','Java','CSS','Python']
@@ -24,13 +25,60 @@ LanguagesNav.propTypes = {
     selected : PropTypes.string.isRequired,
     onUpdateLanguage : PropTypes.func.isRequired
 }
+
+function ReposGrid({repos}){
+    return (
+        <ul className='grid space-around'>
+        {repos.map((repo, index) => {
+          const { name, owner, html_url, stargazers_count, forks, open_issues } = repo
+          const { login, avatar_url } = owner
+  
+          return (
+            <li key={html_url} className='repo bg-light'>
+              <h4 className='header-lg center-text'>
+                #{index + 1}
+              </h4>
+              <img
+                className='avatar'
+                src={avatar_url}
+                alt={`Avatar for ${login}`}
+              />
+              <h2 className='center-text'>
+                <a className='link' href={html_url}>{login}</a>
+              </h2>
+              <ul className='card-list'>
+                <li>
+                  <FaUser color='rgb(255, 191, 116)' size={22} />
+                  <a href={`https://github.com/${login}`}>
+                    {login}
+                  </a>
+                </li>
+                <li>
+                  <FaStar color='rgb(255, 215, 0)' size={22} />
+                  {stargazers_count.toLocaleString()} stars
+                </li>
+                <li>
+                  <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+                  {forks.toLocaleString()} forks
+                </li>
+                <li>
+                  <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
+                  {open_issues.toLocaleString()} open
+                </li>
+              </ul>
+            </li>
+          )
+        })}
+      </ul>
+    )  
+}
 export default class Popular extends React.Component{
     constructor(props){
         super(props)
 
         this.state = {
             selectedLanguage : "All",
-            repos : null,
+            repos : {},
             error : null
         }
         
@@ -40,29 +88,41 @@ export default class Popular extends React.Component{
     }
 
     isLoading(){
-        return this.state.repos === null && this.state.error ===null
+        const {selectedLanguage,repos,error} = this.state
+        return !repos[selectedLanguage] && error === null
     }
 
+    componentDidMount () {
+        this.updateLanguage(this.state.selectedLanguage)
+    }
+    
     updateLanguage(selectedLanguage){
         this.setState({
             selectedLanguage,
-            repos : null,
             error : null
         })
 
-        fetchPopularRepos(selectedLanguage)
-        .then((repos)=>this.setState({
-            repos,
-            error : null
-        }))
-        .catch(()=>{
-            console.warn("Error fetching repose: ",error)
-            this.setState({
-                    error : 'There was an error fetching the repositoy'
+        if(!this.state.repos[selectedLanguage]){
+            fetchPopularRepos(selectedLanguage)
+            .then((data)=> {
+                this.setState(({repos})=>({
+                    repos : {
+                        ...repos,
+                        [selectedLanguage] : data
+                    }
+                }))
             })
-        })
+            .catch(() => {
+                console.warn('Error fetching repos: ', error)
+      
+                this.setState({
+                  error: `There was an error fetching the repositories.`
+                })
+              })
+        }
     }
     render(){
+
         const { selectedLanguage, repos, error } = this.state
 
         return (
@@ -73,7 +133,7 @@ export default class Popular extends React.Component{
                 />
             {this.isLoading() && <p>Loading</p>}
             {error && <p>{error}</p>}
-        {repos && <pre>{JSON.stringify(repos,null,2)}</pre>}
+            {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]}/>}
             </React.Fragment>
         )
     }

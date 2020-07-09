b113ca191559517ddddd9c50e9848ff0d07472fe
Saumya Pandey <sp160899@gmail.com>
2020-07-09 18:53:52 +0530

    Fix build bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
 const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    entry : './app/index.js',
-    output : {
-        path : path.resolve(__dirname,'dist'),
-        filename : 'index_bundle.js',
-        publicPath: '/'
-    },
-    module:{
-        rules:[
-            {test: /\.(js)$/, use: 'babel-loader'},
-            {test: /\.css$/,use: ['style-loader','css-loader']}
-        ]
-    },
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template : 'app/index.html'
-        }),
-        new CopyPlugin([{
-            from : '_redirects'
-        }])
-    ],
-    devServer : {
-        historyApiFallback: true
-    }
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+    ]
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    }),
+    new CopyPlugin({
+        patterns: [
+          { from: '_redirects', to: 'dest' },
+        ],
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
 }
